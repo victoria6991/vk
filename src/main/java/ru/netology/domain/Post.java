c4aea4da1e239e3c7062d7cec2ccdcbe17e19aca
victoria6991 <miss.victoria-liar@yandex.ru>
2022-01-19 21:46:22 +0300

    initail commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Post {
+    private String postName;
+    private int postDateAndTime;
+    private String postContent;
+
+// getters + setters;
+
+}
---
+++ src/main/java/ru/netology/domain/PostFooter.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class PostFooter {
+    private int likesInfo;
+    private String commentsInfo;
+    private String repostsInfo;
+    private int viewsAmount;
+
+    // getters + setters;
+}
